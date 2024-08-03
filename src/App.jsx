b1c0381242b967dc6d171d20287f8ca4ffec28b2
Bisesh-exe <biseshgautam300@gmail.com>
2024-08-03 23:20:45 +0545

    update config

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,14 @@ import Home from './Home.jsx';
 import Navbar from './Components/Navbar.jsx';
 import Footer from './Components/Footer.jsx';
 
-const backgroundStyle = {
-  backgroundImage: 'url(https://www.trios.com/uploads/2023/01/what-is-a-legal-assistant.jpg)',
-  backgroundSize: 'cover',
-  backgroundPosition: 'center',
-  backgroundRepeat: 'no-repeat',
-  height: '100vh',
-  width: '100vw'
-};
 
 function App() {
   return (
-    <div className="d-flex flex-column h-100" style={backgroundStyle}>
+<>
       <Navbar />
       <Home />
       <Footer className="mt-auto" />
-    </div>
+      </>
   );
 }
 
