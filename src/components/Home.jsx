b185be8114b2936e566e3cf446375e3c10a76337
Bisesh-exe <biseshgautam300@gmail.com>
2024-08-05 23:01:31 +0545

    updated home

---
+++ src/components/Home.jsx
@@ -0,0 +1,18 @@
+import "../CSS/Home.css";
+
+function Home() {
+  return (
+    <>
+      <div className="home1">
+        <div className="text1">
+          <h1>S&S Legal and Conveyancing</h1>
+          <h3>Legal solutions to every matter</h3>
+        <button className="button1">Know more</button>
+        </div>
+           
+      </div>
+    </>
+  );
+}
+
+export default Home;
