a04eb53244410a5a919e48c04fb9208f864df8f0
bisesh-exe <biseshgautam300@gmail.com>
2024-08-08 17:44:15 +0545

    update

--- src/components/Getquote.jsx
+++ src/components/Getquote.jsx
@@ -18,12 +18,25 @@ function Getquote() {
         <label htmlFor="email" className="label">Email</label>
         <input type="email" id="email" required />
 
-        <label htmlFor="services" className="label">Services</label>
+        <label htmlFor="services" className="label">Buy, Sell or Transfer</label>
         <select id="services" required>
-          <option value="">Select a service</option>
-          <option value="FamilyLaw">Family Law</option>
-          <option value="CriminalLaw">Criminal Law</option>
-          <option value="BusinessLaw">Business Law</option>
+          <option value="">Select</option>
+          <option value="Buy">Buy</option>
+          <option value="Sell">Sell</option>
+          <option value="Transfer">Transfer</option>
+        </select>
+
+        <label htmlFor="services" className="label">Property Type</label>
+        <select id="services" required>
+          <option value="">Select a Property</option>
+          <option value="House">House</option>
+          <option value="Strataunit">Strata Unit</option>
+          <option value="Offtheplan">Off the plan</option>
+          <option value="Vacantland">Vacant Land</option>
+          <option value="Companytitle">Company Title</option>
+          <option value="Commercialbuilding">Commercial Building</option>
+          <option value="Commerciallease">Commercial Lease</option>
+          <option value="Retaillease">Retail Lease</option>
         </select>
 
         <input type="submit" value="Submit" className="submit-btn" />
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,17 @@
 import "../CSS/Home.css";
-import familySvg from "../assets/family.svg";
-import crimeSvg from "../assets/crime.svg";
-import businessSvg from "../assets/business.svg";
+import homeSvg from "../assets/home.svg";
+import unitSvg from "../assets/unit.svg";
+import rentSvg from "../assets/rent.svg";
+import sachinpng from "../assets/sachin.png";
 
 import { Link } from 'react-router-dom';
 
 function Home() {
 
- 
+
   return (
     <>
-    {/* parallex */}
+      {/* parallex */}
       <div className="home1">
         <div className="text1">
           <h1>S&S Legal and Conveyancing</h1>
@@ -19,25 +20,42 @@ function Home() {
         </div>
       </div>
 
-{/* Services */}
-<h1 className="home2-h1">Our Expertise</h1>
+      {/* Expertise */}
+      <h1 className="home2-h1">Our Expertise</h1>
       <div className="home2">
-      <div className="text2">
-          <img src={familySvg} alt="Family law" />
-          <h3>Family Law</h3>
+        <div className="text2">
+          <img src={homeSvg} alt="Family law" />
+          <h3>Buy, Sell Home</h3>
           <p>At S&S Legal, we specialize in family law, providing expert guidance and compassionate support for all family-related legal matters.</p>
         </div>
         <div className="text2">
-          <img src={crimeSvg} alt="Crime law" />
-          <h3>Criminal Law</h3>
+          <img src={unitSvg} alt="Crime law" />
+          <h3>Unit Lease</h3>
           <p>Our experienced team at S&S Legal offers dedicated criminal law services, ensuring robust defense and fair representation in all criminal cases.</p>
         </div>
         <div className="text2">
-          <img src={businessSvg} alt="Business law" />
-          <h3>Business Law</h3>
+          <img src={rentSvg} alt="Business law" />
+          <h3>Rent Land</h3>
           <p>S&S Legal excels in business law, delivering comprehensive legal solutions to protect and enhance your business interests.</p>
         </div>
       </div>
+
+
+
+      {/* Why us */}
+      
+      <div className="home3">
+  <h1 className="home3-h1">Why us?</h1>
+  <div className="text3">
+    <img className="sachinsir"src={sachinpng} alt="sachinsir pic" />
+  </div>
+  <div className="text4">
+    <h2>Sachin Katuwal's diverse expertise as a Conveyancer, Financial Accountant, and Business Analyst</h2>
+    <p>We are thrilled to announce that Sachin Katuwal has joined us as our new Business Manager and Analyst. Sachin has a vast amount of experience in both the private and the public sector with his most recent work being a Business Analyst at the iconic Sydney Opera House under the Australian Government.</p>
+  </div>
+</div>
+
+
     </>
   );
 }
